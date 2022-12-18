e08000b121ac36c57760bf8b4200ed350ff692aa
lkalliance <119076402+lkalliance@users.noreply.github.com>
2022-12-18 11:10:04 -0600

    Some code cleanup and comments

--- script.js
+++ script.js
@@ -7,22 +7,24 @@ const special = ["!","@","#","$","%","^","&","(",")","+","=","-","_","~"];
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+/* ----- FUNCTIONS ----- */
+
 // Write password to the #password input
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
+// Generate and return a new password, or a message why user needs to try again
 function generatePassword() {
-  // This function starts and ends the process for generating
-  // the new password, including getting the user's preferences
+  
   let newPassword = "";
 
   // Collect the user's preferences for each kind of character
@@ -31,20 +33,15 @@ function generatePassword() {
   let useNumerals = confirm('Would you like to use numbers in your password? Click "OK" for yes, "Cancel" for no.');
   let useSpecial = confirm('Would you like to use special characters in your password? Click "OK" for yes, "Cancel" for no.');
 
-  // Create reference object to pass to character generator
-  let kindsOfCharacters = {
-    lowerCase: useLowerCase,
-    upperCase: useUpperCase,
-    numerals: useNumerals,
-    special: useSpecial
-  }
   
   // Test to make sure the user picked at least one kind of character
   // If they didn't, return a pithy response
   if(!(useLowerCase || useUpperCase || useNumerals || useSpecial)) return "Well you have to use SOME kind of character! Try again or leave before I contact the authorities.";
+
   
   // Ask the user how long the password should be
   let howMany = prompt("Finally, how long would you like your password? Enter the number of characters here.");
+
 
   // Check to make sure the length value is valid
   // If it isn't, return a pithy response
@@ -53,13 +50,15 @@ function generatePassword() {
 
   /* IF WE'VE MADE IT THIS FAR, WE HAVE VALID PARAMETERS */
 
+
   // Concatenate the relevant arrays
   let noCharacters = [];
-  let allAvailableCharacters = noCharacters.concat(useLowerCase?letters:[], useUpperCase?upperLetters:[], useNumerals?numbers:[], useSpecial?special:[]);
+  let allAvailable = noCharacters.concat(useLowerCase?letters:[], useUpperCase?upperLetters:[], useNumerals?numbers:[], useSpecial?special:[]);
+  
 
   // Assemble the password
   for(let i = 0; i < howMany; i++) {
-    newPassword += allAvailableCharacters[Math.floor(Math.random() * allAvailableCharacters.length)];
+    newPassword += allAvailable[Math.floor(Math.random() * allAvailable.length)];
   }
  
   return newPassword;
